f6622933c0fc1a2dcd3d76f5296bdf456d1300b5
PeterG <98351061+PeterG-ithub@users.noreply.github.com>
2024-02-12 13:55:22 -0800

    add enemy spawner

---
+++ enemy/enemy_spawner.gd
@@ -0,0 +1,18 @@
+extends Node2D
+
+
+const MAX_ENEMY_COUNT = 10
+const ENEMY = preload("res://enemy/enemy.tscn")
+
+@onready var main = $".."
+@onready var path_follow_2d = $Path2D/PathFollow2D
+
+func spawn_enemy():
+	var enemy = ENEMY.instantiate()
+	enemy.died.connect(main.enemy_dead)
+	path_follow_2d.progress_ratio = randf()
+	enemy.global_position = path_follow_2d.global_position
+	main.add_child(enemy)
+
+func _on_spawn_timer_timeout():
+	spawn_enemy()
