fa98b74523d4558f44667ba17307861311389436
PeterG <98351061+PeterG-ithub@users.noreply.github.com>
2024-02-12 12:55:04 -0800

    add healthbar to enemy

--- enemy/enemy.gd
+++ enemy/enemy.gd
@@ -1,8 +1,14 @@
 extends CharacterBody2D
 
 @onready var player = $"../Player"
+@onready var health_bar = $HealthBar
 
-var health = 100
+const MAX_HEALTH = 1000.0
+var health
+
+func _ready():
+	health = MAX_HEALTH
+	health_bar.value = health
 
 func _physics_process(delta):
 	var direction = global_position.direction_to(player.global_position)
@@ -12,6 +18,8 @@ func _physics_process(delta):
 
 func take_damage(damage):
 	health -= damage
+	health_bar.value = (health / MAX_HEALTH * 100)
 	print("%s has taken %s damage" % [self, damage])
 	if health <= 0:
 		queue_free()
+
