c272d640780880e21c1707a94c4cb88b3b0cbeb7
PeterG <98351061+PeterG-ithub@users.noreply.github.com>
2024-02-12 14:10:49 -0800

    add level up function

--- main.gd
+++ main.gd
@@ -2,7 +2,8 @@ extends Node
 
 var exp = 0.0
 var level_up_exp = 100.0
-
+var level = 1
+@onready var level_label = $UI/ExperienceBar/LevelLabel
 @onready var experience_bar = $UI/ExperienceBar
 
 func _ready():
@@ -10,10 +11,17 @@ func _ready():
 	
 
 func enemy_dead():
-	print("enemy dead")
 	exp += 20.0
-	print(exp)
 	experience_bar.value = exp
-	
-
+	if exp >= level_up_exp:
+		level_up()
+		
+func level_up():
+	print("level up")
+	level += 1
+	level_up_exp = level_up_exp * 1.5
+	exp = 0.0
+	experience_bar.max_value = level_up_exp
+	experience_bar.value = exp
+	level_label.text = "Lv.%s" % level
 	
