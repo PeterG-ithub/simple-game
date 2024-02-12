14ea377292a5ee6693376dd7a7ba4051767ec223
PeterG <98351061+PeterG-ithub@users.noreply.github.com>
2024-02-12 14:55:24 -0800

    add simple main menu

---
+++ ui/main_menu.gd
@@ -0,0 +1,6 @@
+extends Control
+@onready var button = $HBoxContainer/VBoxContainer/Button
+const MAIN = preload("res://main.tscn")
+func _on_button_pressed():
+	var main = MAIN.instantiate()
+	get_tree().root.add_child(main)
