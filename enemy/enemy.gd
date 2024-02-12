5cdeaec7195890fb1d4e17cf3080851a179e9598
PeterG <98351061+PeterG-ithub@users.noreply.github.com>
2024-02-12 12:45:51 -0800

    configure projectile to detect enemy
    add new function take_damage(damage)
    in enemy

--- enemy/enemy.gd
+++ enemy/enemy.gd
@@ -2,9 +2,16 @@ extends CharacterBody2D
 
 @onready var player = $"../Player"
 
+var health = 100
 
 func _physics_process(delta):
 	var direction = global_position.direction_to(player.global_position)
 	velocity = direction * 50
 	
 	move_and_collide(velocity * delta) 
+
+func take_damage(damage):
+	health -= damage
+	print("%s has taken %s damage" % [self, damage])
+	if health <= 0:
+		queue_free()
--- player/player.gd
+++ player/player.gd
@@ -6,17 +6,15 @@ const PROJECTILE = preload("res://projectile/projectile.tscn")
 func _physics_process(delta):
 	var direction = Input.get_vector("left","right","up","down")
 	velocity = direction * 200
-	
 	move_and_collide(velocity * delta)
 
 func _input(event):
-	
 	if event.is_action_pressed("shoot"):
 		shoot()
 
 
 func shoot():
 	var projectile = PROJECTILE.instantiate()
-	projectile.global_position = global_position
+	projectile.global_position = shooting_point.global_position
+	projectile.global_rotation = shooting_point.global_position.direction_to(get_global_mouse_position()).angle()
 	shooting_point.add_child(projectile)
-	print("shoot")
