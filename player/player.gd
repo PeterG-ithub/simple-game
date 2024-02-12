3bac9a4df5ea096a749de966d52bb26954332277
PeterG <98351061+PeterG-ithub@users.noreply.github.com>
2024-02-12 13:13:42 -0800

    add healthbar to player

--- player/player.gd
+++ player/player.gd
@@ -1,7 +1,16 @@
 extends CharacterBody2D
 
 const PROJECTILE = preload("res://projectile/projectile.tscn")
+const MAX_HEALTH = 100.0
+
+var health = 0.0
+
 @onready var shooting_point = $ShootingPoint
+@onready var health_bar = $HealthBar
+
+func _ready():
+	health = MAX_HEALTH
+	health_bar.value = health
 
 func _physics_process(delta):
 	var direction = Input.get_vector("left","right","up","down")
@@ -18,3 +27,10 @@ func shoot():
 	projectile.global_position = shooting_point.global_position
 	projectile.global_rotation = shooting_point.global_position.direction_to(get_global_mouse_position()).angle()
 	shooting_point.add_child(projectile)
+
+func take_damage(damage):
+	health -= damage
+	health_bar.value = (health / MAX_HEALTH * 100)
+	print("%s has taken %s damage" % [self, damage])
+	if health <= 0:
+		queue_free()
