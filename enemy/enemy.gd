e6934aa7ad2f9ebd606f873d5963b25a9be8e6ed
PeterG <98351061+PeterG-ithub@users.noreply.github.com>
2024-02-12 13:13:54 -0800

    add attack cooldown to enemy

--- enemy/enemy.gd
+++ enemy/enemy.gd
@@ -2,6 +2,8 @@ extends CharacterBody2D
 
 @onready var player = $"../Player"
 @onready var health_bar = $HealthBar
+@onready var attack_cooldown = $AttackCooldown
+@onready var hitbox = $Hitbox
 
 const MAX_HEALTH = 1000.0
 var health
@@ -9,11 +11,10 @@ var health
 func _ready():
 	health = MAX_HEALTH
 	health_bar.value = health
-
+	
 func _physics_process(delta):
 	var direction = global_position.direction_to(player.global_position)
 	velocity = direction * 50
-	
 	move_and_collide(velocity * delta) 
 
 func take_damage(damage):
@@ -23,3 +24,11 @@ func take_damage(damage):
 	if health <= 0:
 		queue_free()
 
+
+
+func _on_hitbox_body_entered(body):
+	if attack_cooldown.is_stopped():
+		if body.has_method("take_damage"):
+			body.take_damage(10)
+		attack_cooldown.start()
+		
