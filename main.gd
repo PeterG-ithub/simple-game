d80a1e490ea8bf8adcb66d731cb7489ceb93c405
PeterG <98351061+PeterG-ithub@users.noreply.github.com>
2024-02-12 15:06:35 -0800

    add winner menu

--- main.gd
+++ main.gd
@@ -7,10 +7,10 @@ var level = 1
 @onready var level_label = $UI/ExperienceBar/LevelLabel
 @onready var experience_bar = $UI/ExperienceBar
 @onready var player = $Player
+@onready var ui = $UI
 
 func _ready():
 	experience_bar.max_value = level_up_exp
-	
 
 func enemy_dead():
 	exp += 20.0
@@ -19,9 +19,10 @@ func enemy_dead():
 		level_up()
 func level_up():
 	print("level up")
-	if level == 10:
-		winner()
 	level += 1
+	if level == 2:
+		print("winner")
+		winner()
 	level_up_exp = level_up_exp * 1.5
 	exp = 0.0
 	experience_bar.max_value = level_up_exp
@@ -29,5 +30,9 @@ func level_up():
 	level_label.text = "Lv.%s" % level
 	player.damage_stat += 50
 
+
+
 func winner():
-	pass
+	var win_scene = preload("res://ui/winner.tscn")
+	ui.add_child(win_scene.instantiate())
+	get_tree().paused = true
