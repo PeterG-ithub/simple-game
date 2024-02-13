6b19442bfd1435fe2795646a5e0280e885b82cce
PeterG <98351061+PeterG-ithub@users.noreply.github.com>
2024-02-13 11:20:27 -0800

    create data containers for inventory
    Inventory contains inventory data, slot data, and item data

---
+++ item/item_data.gd
@@ -0,0 +1,9 @@
+class_name ItemData
+extends Resource
+
+@export var name: String = ""
+@export_enum("Inv", "Weapon", "Ring") var item_type: String
+
+@export var health: float = 0.0
+@export var speed: float = 0.0
+@export var damage: float = 0.0
