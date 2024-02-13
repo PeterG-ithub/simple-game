b216a29f278743bff7778acbe756ab4a72166220
PeterG <98351061+PeterG-ithub@users.noreply.github.com>
2024-02-13 15:18:55 -0800

    create slot script with some functions
    func set_slot_data changes the texture of the slot base on item_data texture.
    func reset texture resets the texture_rect of slot
    func on_gui_input emits a signal on which slot was click

---
+++ inventory/slot.gd
@@ -0,0 +1,18 @@
+extends PanelContainer
+
+signal slot_clicked(index: int, button: int)
+
+@export_enum("Inv", "Weapon", "Ring") var slot_type: String
+
+@onready var texture_rect = $MarginContainer/TextureRect
+
+func set_slot_data(slot_data: SlotData):
+	texture_rect.texture = slot_data.item_data.texture
+
+func reset_texture():
+	texture_rect.texture = null
+
+func _on_gui_input(event):
+	if event is InputEventMouseButton \
+			and event.pressed:
+		slot_clicked.emit(get_index(), event.button_index)
