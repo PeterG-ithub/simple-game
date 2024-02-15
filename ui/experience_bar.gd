939b0974e2114339b9d127daa9810aca3550c579
PeterG <98351061+PeterG-ithub@users.noreply.github.com>
2024-02-14 17:31:01 -0800

    Move max_value assignment from main

--- ui/experience_bar.gd
+++ ui/experience_bar.gd
@@ -2,6 +2,9 @@ extends TextureProgressBar
 
 @onready var player_variables: PlayerVariable = get_node("/root/PlayerVariable")
 
+func _ready():
+	max_value = player_variables.level_up_exp
+
 func update_experience_bar():
 	max_value = player_variables.level_up_exp
 	value = player_variables.experience
