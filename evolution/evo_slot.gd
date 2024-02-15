6b4607dd68b0a0f9ee75e2185a56de904f1c20e4
PeterG <98351061+PeterG-ithub@users.noreply.github.com>
2024-02-15 13:34:51 -0800

    Refactor slot script and ui script
    Slot script will now contain the node data for easier node calling.

--- evolution/evo_slot.gd
+++ evolution/evo_slot.gd
@@ -2,7 +2,12 @@ extends PanelContainer
 
 signal slot_clicked(index, button)
 
+@export var evolution_node: EvolutionNode
+
 @onready var texture_rect = $MarginContainer/TextureRect
+
+func initialize_slot():
+	set_slot_texture(evolution_node)
 
 func set_slot_texture(evo_node: EvolutionNode):
 	texture_rect.texture = evo_node.texture
@@ -11,3 +16,4 @@ func _on_gui_input(event):
 	if event is InputEventMouseButton \
 			and event.pressed:
 		slot_clicked.emit(get_index(), event.button_index)
+
--- evolution/evo_ui.gd
+++ evolution/evo_ui.gd
@@ -11,10 +11,13 @@ func initialize_interface(evolution_tree: EvolutionTree):
 	var current_node = evo_tree.starting_node
 	var slots = evo_slots.get_children()
 	for index in range(3): 
-		slots[index].set_slot_texture(current_node)
+		slots[index].evolution_node = current_node
+		slots[index].initialize_slot()
 		slots[index].slot_clicked.connect(on_slot_clicked)
 		if current_node.next_evo:
 			current_node = current_node.next_evo
 
 func on_slot_clicked(index, button):
+	var slots = evo_slots.get_children()
+	print(slots[index].evolution_node.name)
 	print("%s %s" % [index, button])
