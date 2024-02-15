04eaab02d318c546b1793efd7934701e77e003f2
PeterG <98351061+PeterG-ithub@users.noreply.github.com>
2024-02-15 12:46:32 -0800

    Add set_slot_texture function

---
+++ evolution/evo_slot.gd
@@ -0,0 +1,8 @@
+extends PanelContainer
+
+@export var evo_node: EvolutionNode
+
+@onready var texture_rect = $MarginContainer/TextureRect
+
+func set_slot_texture(evo_node: EvolutionNode):
+	texture_rect.texture = evo_node.texture
