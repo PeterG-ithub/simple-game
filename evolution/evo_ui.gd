547c9660e962ae347e00fed143fe6a4a31413030
PeterG <98351061+PeterG-ithub@users.noreply.github.com>
2024-02-15 13:08:09 -0800

    add initialize evo tree

--- evolution/evo_ui.gd
+++ evolution/evo_ui.gd
@@ -1,1 +1,16 @@
 extends Control
+
+@export var evo_tree: EvolutionTree
+
+@onready var evo_slots = $MarginContainer/PanelContainer/NinePatchRect/EvoSlots
+
+func _ready():
+	initialize_interface(evo_tree)
+
+func initialize_interface(evolution_tree: EvolutionTree):
+	var current_node = evo_tree.starting_node
+	var slots = evo_slots.get_children()
+	for index in range(3): 
+		slots[index].set_slot_texture(current_node)
+		if current_node.next_evo:
+			current_node = current_node.next_evo
