30cc1ab1d9b890ddbb61ee9b02c274d519fcc57a
PeterG <98351061+PeterG-ithub@users.noreply.github.com>
2024-02-15 13:15:26 -0800

    Add slot clicked signal
    The signal will emit when there is a mouse button click on the slot

--- evolution/evo_slot.gd
+++ evolution/evo_slot.gd
@@ -1,6 +1,13 @@
 extends PanelContainer
 
+signal slot_clicked(index, button)
+
 @onready var texture_rect = $MarginContainer/TextureRect
 
 func set_slot_texture(evo_node: EvolutionNode):
 	texture_rect.texture = evo_node.texture
+
+func _on_gui_input(event):
+	if event is InputEventMouseButton \
+			and event.pressed:
+		slot_clicked.emit(get_index(), event.button_index)
