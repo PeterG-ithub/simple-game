53a5958f89f4985ee105d5c7042da2897a0be950
PeterG <98351061+PeterG-ithub@users.noreply.github.com>
2024-02-19 10:06:44 -0800

    Update on_slot_clicked and initialize_int
    Update initialize interface to change the player_variable current_evolution.
    .Update on_slot_clicked will call the check_evo_req function with player_variable as arguments

--- evolution/evo_ui.gd
+++ evolution/evo_ui.gd
@@ -3,12 +3,14 @@ extends Control
 @export var evo_tree: EvolutionTree
 
 @onready var evo_slots = $MarginContainer/PanelContainer/NinePatchRect/EvoSlots
+@onready var player_variables: PlayerVariable = get_node("/root/PlayerVariable")
 
 func _ready():
 	initialize_interface(evo_tree)
 
 func initialize_interface(evolution_tree: EvolutionTree):
 	var current_node = evo_tree.starting_node
+	player_variables.current_evolution = current_node.name
 	var slots = evo_slots.get_children()
 	for index in range(3): 
 		slots[index].evolution_node = current_node
@@ -16,8 +18,9 @@ func initialize_interface(evolution_tree: EvolutionTree):
 		slots[index].slot_clicked.connect(on_slot_clicked)
 		if current_node.next_evo:
 			current_node = current_node.next_evo
-
+	
 func on_slot_clicked(index, button):
 	var slots = evo_slots.get_children()
-	print(slots[index].evolution_node.name)
-	print("%s %s" % [index, button])
+	slots[index].evolution_node.check_evo_requirement(player_variables)
+	#print(slots[index].evolution_node.name)
+	#print("%s %s" % [index, button])
